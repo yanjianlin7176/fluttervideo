7ae7a63ece4b01bc3e36293d51dc31ed423f9d71
zhouliutao <zhouliutao@iauto.com>
2019-09-18 17:48:05 +0800

    1.增加资源文件和登陆页面

--- flutterhello/lib/main.dart
+++ flutterhello/lib/main.dart
@@ -3,7 +3,10 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
+import 'login_page.dart';
+
+
 
 void main() => runApp(MyApp());
 
@@ -11,46 +14,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: 'Startup Name Generator',
-    home: RandomWords(),
+    debugShowCheckedModeBanner: false,  
+    title: 'flutter',
+    theme: ThemeData(
+    ),
+    home: MyHomePage(title:'主页'),
+    routes: {
+      'LoginPage': (context) => LoginPage(),
+    },
     );
   }
 }
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Startup Name Generator'),
-    ),
-    body: _buildSuggestions(),
-  );
-}
-  Widget _buildSuggestions(){
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-       if(i.isOdd) return Divider();
-       final index = i ~/2;
-       if(index >= _suggestions.length){
-         _suggestions.addAll(generateWordPairs().take(10));
-       }
-       return _buildRow(_suggestions[index]);
-      });
-  }
-  Widget _buildRow(WordPair pair) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-  );
-}
-  
-}
-class RandomWords extends StatefulWidget {
+
+class MyHomePage extends StatefulWidget{
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
   @override
-  RandomWordsState createState() => RandomWordsState();
+  _MyHomePageState createState() => _MyHomePageState();
+  }
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Demo'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: (){
+                  navigateTo('LoginPage');
+                },
+                child: Text('登录页面'),
+              )
+            ],
+          )),
+
+        ),
+        
+      ),
+   );
+  }
+  
+  navigateTo(name) {
+    Navigator.of(context).pushNamed(name);
+  }
 }
